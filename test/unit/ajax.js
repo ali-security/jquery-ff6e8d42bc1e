9072b2e8569de416923871aee3660f63a93908a5
Seal <info@sealsecurity.io>
2026-09-16 08:48:22 +0000

    CVE-2015-9251

--- test/unit/ajax.js
+++ test/unit/ajax.js
@@ -60,6 +60,54 @@ module( "ajax", {
 		}
 	});
 
+	ajaxTest( "jQuery.ajax() - do not execute js (crossOrigin)", 2, function() {
+		return {
+			create: function( options ) {
+				options.crossDomain = true;
+				return jQuery.ajax( url("data/script.php?header=ecma"), options );
+			},
+			success: function() {
+				ok( true, "success" );
+			},
+			complete: function() {
+				ok( true, "complete" );
+			}
+		};
+	});
+
+	ajaxTest( "jQuery.ajax() - execute js for crossOrigin when dataType option is provided", 3,
+		function() {
+			return {
+				create: function( options ) {
+					options.crossDomain = true;
+					options.dataType = "script";
+					return jQuery.ajax( url("data/script.php?header=ecma"), options );
+				},
+				success: function() {
+					ok( true, "success" );
+				},
+				complete: function() {
+					ok( true, "complete" );
+				}
+			};
+		}
+	);
+
+	ajaxTest( "jQuery.ajax() - do not execute js (crossOrigin) - no content type", 2, function() {
+		return {
+			create: function( options ) {
+				options.crossDomain = true;
+				return jQuery.ajax( url("data/script.php"), options );
+			},
+			success: function() {
+				ok( true, "success" );
+			},
+			complete: function() {
+				ok( true, "complete" );
+			}
+		};
+	});
+
 	ajaxTest( "jQuery.ajax() - success callbacks (late binding)", 8, {
 		setup: addGlobalEvents("ajaxStart ajaxStop ajaxSend ajaxComplete ajaxSuccess"),
 		url: url("data/name.html"),
